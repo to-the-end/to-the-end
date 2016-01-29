0f58ae131a1c71f43adc50c6fc8ce8b5b89e8267
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-01-29 19:40:35 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const electron = require('electron');
+
+let mainWindow;
+
+const createWindow = function createWindow() {
+  mainWindow = new electron.BrowserWindow({
+    title:      'GGJ 16',
+    fullscreen: true,
+    frame:      false,
+  });
+
+  mainWindow.loadURL(`file://${__dirname}/index.html`);
+
+  mainWindow.on('closed', function onClosed() {
+    mainWindow = null;
+  });
+};
+
+electron.app.on('ready', createWindow);
+
+electron.app.on('window-all-closed', function onWindowAllClosed() {
+  if (process.platform !== 'darwin') {
+    electron.app.quit();
+  }
+});
+
+electron.app.on('activate', function onActivate() {
+  if (!mainWindow) {
+    createWindow();
+  }
+});
