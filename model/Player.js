d79fbf4e1bae18c3b0319461660d33d9ed3e5c76
Michael Le <hle.michael@gmail.com>
2016-01-29 23:17:56 +0000

    removing original player

--- model/Player.js
+++ model/Player.js
@@ -1,13 +1,13 @@
 'use strict';
 
-class Player {
+class Player extends Phaser.Sprite {
 
-  constructor(game, x, y) {
-    this.game = game;
-    this.sprite = game.add.sprite(0, 0, 'dude');
+  constructor(game, x, y, frame) {
+    super(game, x, y, frame);
+    this.sprite = this.game.add.sprite(0, 0, 'dude');
     this.game.physics.arcade.enable(this.sprite);
     this.sprite.body.collideWorldBounds = true;
-
+    this.sprite.body.setSize(10, 14, 2, 1);
     this.sprite.animations.add('left', [0, 1, 2, 3], 10, true);
     this.sprite.animations.add('right', [5, 6, 7, 8], 10, true);
   }
--- states/main.js
+++ states/main.js
@@ -3,16 +3,13 @@
 
 let Player = require('../model/Player');
 
-let cursors;
-let player;
-
 module.exports = {
   init() {
 
   },
 
   preload() {
-    cursors = this.input.keyboard.createCursorKeys();
+
   },
 
   create() {
@@ -25,45 +22,28 @@ module.exports = {
     this.layer = map.createLayer(0);
     this.layer.resizeWorld();
     // Add character
-	player = new Player(this, 0, 0);
-    this.player = this.game.add.sprite(48, 48, 'player', 1);
-    this.player.animations.add('left', [8,9], 10, true);
-    this.player.animations.add('right', [1,2], 10, true);
-    this.player.animations.add('up', [11,12,13], 10, true);
-    this.player.animations.add('down', [4,5,6], 10, true);
-    // Enable physics on character
-    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
-    this.player.body.setSize(10, 14, 2, 1);
+    this.player = new Player(this, 0, 0);
+    
     // Follow character
-    this.game.camera.follow(this.player);
+    this.camera.follow(this.player);
     // Create cursor keys
     this.cursors = this.game.input.keyboard.createCursorKeys();
   },
 
   update() {
-	player.resetVelocity();
+    this.player.resetVelocity();
     this.game.physics.arcade.collide(this.player, this.layer);
-    this.player.body.velocity.set(0);
 
     if (this.cursors.left.isDown) {
-      this.player.body.velocity.x = -100;
-      this.player.play('left');
-	  player.walkLeft();
+      this.player.walkLeft();
     } else if (this.cursors.right.isDown) {
-      this.player.body.velocity.x = 100;
-      this.player.play('right');
-	  player.walkRight();
+      this.player.walkRight();
     } else if (this.cursors.up.isDown) {
-      this.player.body.velocity.y = -100;
-      this.player.play('up');
-      player.walkUp();
+      this.player.walkUp();
     } else if (this.cursors.down.isDown) {
-      this.player.body.velocity.y = 100;
-      this.player.play('down');
-      player.walkDown();
+      this.player.walkDown();
     } else {
-      this.player.animations.stop();
-      player.stop();
+      this.player.stop();
     }
   },
 
