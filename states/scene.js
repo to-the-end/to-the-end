e99d457cb4fa1018567a83766583f3b926741908
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-01-31 13:12:05 +0000

    Allow images in the scenes

--- states/scene.js
+++ states/scene.js
@@ -32,80 +32,107 @@ module.exports = {
       this.dialogueGroup.removeAll(true);
     }
 
-    let x;
-    let y;
-    let anchorX;
-    let anchorY;
-    let alignH;
-    let alignV;
+    let sprite;
 
-    if (dialogue.position === 'tl') {
-      x = 0;
-      y = 0;
-      anchorX = 0;
-      anchorY = 0;
-      alignH = 'left';
-      alignV = 'top';
-    } else if (dialogue.position === 'tr') {
-      x = this.camera.view.width;
-      y = 0;
-      anchorX = 1;
-      anchorY = 0;
-      alignH = 'right';
-      alignV = 'top';
-    } else if (dialogue.position === 'bl') {
-      x = 0;
-      y = this.camera.view.height;
-      anchorX = 0;
-      anchorY = 1;
-      alignH = 'left';
-      alignV = 'bottom';
-    } else if (dialogue.position === 'br') {
-      x = this.camera.view.width;
-      y = this.camera.view.height;
-      anchorX = 1;
-      anchorY = 1;
-      alignH = 'right';
-      alignV = 'bottom';
-    } else {
-      x = this.camera.view.width / 2;
-      y = this.camera.view.height / 2;
-      anchorX = 0.5;
-      anchorY = 0.5;
-      alignH = 'center';
-      alignV = 'center';
+    if (dialogue.image) {
+      sprite = this.add.sprite(
+        this.camera.view.width / 2, this.camera.view.height / 2, dialogue.image
+      );
+
+      sprite.anchor.set(0.5);
+
+      const scale = Math.max(
+        this.camera.view.width / sprite.width,
+        this.camera.view.height / sprite.height
+      );
+
+      sprite.scale.set(scale);
+
+      this.dialogueGroup.add(sprite);
     }
 
-    const style = {
-      font:     'Raleway',
-      fontSize: 24,
+    let text;
 
-      fill: '#fff',
+    if (dialogue.text) {
+      let x;
+      let y;
+      let anchorX;
+      let anchorY;
+      let alignH;
+      let alignV;
 
-      stroke:          '#000',
-      strokeThickness: 3,
+      if (dialogue.position === 'tl') {
+        x = 0;
+        y = 0;
+        anchorX = 0;
+        anchorY = 0;
+        alignH = 'left';
+        alignV = 'top';
+      } else if (dialogue.position === 'tr') {
+        x = this.camera.view.width;
+        y = 0;
+        anchorX = 1;
+        anchorY = 0;
+        alignH = 'right';
+        alignV = 'top';
+      } else if (dialogue.position === 'bl') {
+        x = 0;
+        y = this.camera.view.height;
+        anchorX = 0;
+        anchorY = 1;
+        alignH = 'left';
+        alignV = 'bottom';
+      } else if (dialogue.position === 'br') {
+        x = this.camera.view.width;
+        y = this.camera.view.height;
+        anchorX = 1;
+        anchorY = 1;
+        alignH = 'right';
+        alignV = 'bottom';
+      } else {
+        x = this.camera.view.width / 2;
+        y = this.camera.view.height / 2;
+        anchorX = 0.5;
+        anchorY = 0.5;
+        alignH = 'center';
+        alignV = 'center';
+      }
 
-      boundsAlignH:  alignH,
-      boundsAlignV:  alignV,
-      wordWrap:      true,
-      wordWrapWidth: this.camera.view.width * 0.6,
-    };
+      const style = {
+        font:     'Raleway',
+        fontSize: 24,
 
-    const text = this.add.text(x, y, '', style);
+        fill: '#fff',
 
-    text.anchor.set(anchorX, anchorY);
-    text.fixedToCamera = true;
+        stroke:          '#000',
+        strokeThickness: 3,
 
-    this.dialogueGroup.add(text);
+        boundsAlignH:  alignH,
+        boundsAlignV:  alignV,
+        wordWrap:      true,
+        wordWrapWidth: this.camera.view.width * 0.6,
+      };
+
+      text = this.add.text(x, y, '', style);
+
+      text.anchor.set(anchorX, anchorY);
+      text.fixedToCamera = true;
+
+      this.dialogueGroup.add(text);
+    }
+
+    const repetitions = dialogue.text ? dialogue.text.length : 1;
 
     let i = 0;
 
-    this.time.events.repeat(15, dialogue.text.length, function updateText() {
+    this.time.events.repeat(15, repetitions, function updateText() {
       i++;
 
-      text.setText(dialogue.text.substring(0, i));
+      if (text) {
+        text.setText(dialogue.text.substring(0, i));
+      }
 
-      if (i >= dialogue.text.length) {
+      if (i >= repetitions) {
         this.time.events.add(Phaser.Timer.SECOND * 2.5, function playNext() {
           this.playDialogue(index + 1);
         }, this);
