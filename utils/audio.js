70afb3fdab204817d3b15f4d9181090bb384645e
Dave Allen <daveallenbpm@gmail.com>
2016-02-05 00:29:03 +0000

    Add some audio functionality.
    Includes:
    - Basic looping
    - Playing sequences (with and without crossfading)
    - Crossfading two sounds

--- utils/audio.js
+++ utils/audio.js
@@ -1,18 +1,103 @@
 'use strict';
 
-module.exports = {
-  buildSfxCollection(game, componentName, clipCount) {
-    // FIXME: Make this a class.
+class Audio {
+  constructor(game){
+    this.game = game;
+  }
+
+  buildSfxCollection(componentName, clipCount) {
     const sounds = [];
 
     for (let i = 0; i < clipCount; i++) {
-      sounds.push(this.addSfx(game, componentName, i));
+      sounds.push(this.addSfx(componentName, i));
     }
 
     return sounds;
-  },
+  }
 
-  addSfx(game, componentName, clipId) {
-    return game.add.audio(`${componentName}-${clipId}-sfx`);
-  },
-};
+  addSfx(componentName, clipId) {
+    return this.game.add.audio(`${componentName}-${clipId}-sfx`);
+  }
+
+  // Play sequence of non-looping sounds
+  playSequence(soundKeys){
+    var sounds = soundKeys.map((key) => {
+      return this.game.add.audio(key);
+    });
+
+    playSoundsInOrder(sounds);
+  }
+
+  playSoundsInOrder(sounds){
+    if (sounds.length > 0){
+      sounds[0].play();
+      sounds[0].onStop.addOnce(() => {
+        playSoundsInOrder(sounds.slice(1));
+      });
+    }
+  }
+
+  playSequenceWithCrossfades(soundKeys, dividingFactor){
+    if (!dividingFactor || dividingFactor < 1){
+      dividingFactor = 10;
+    }
+
+    var sounds = soundKeys.map((key) => {
+      return this.game.add.audio(key);
+    });
+
+    this.playSoundsInOrderWithCrossfades(sounds, dividingFactor);
+  }
+
+  playSoundsInOrderWithCrossfades(sounds, dividingFactor){
+    if (sounds.length > 1){
+      var sound1 = sounds[0];
+      var sound2 = sounds[1];
+
+      if (!sound1.isPlaying){
+        sound1.play();
+      }
+
+      var duration = sound1.totalDuration * 1000;
+      var fadeBoundary = duration / dividingFactor;
+      var tween = this.game.add.tween(sound1);
+      tween.to({ volume: 0 }, fadeBoundary, Phaser.Easing.Linear.None, false);
+
+      setTimeout(() => {
+        tween.start();
+        sound2.fadeIn(fadeBoundary);
+        this.playSoundsInOrderWithCrossfades(sounds.slice(1), dividingFactor);
+      }, duration - fadeBoundary);
+    } else if (sounds.length === 1){
+      // do nothing?
+    } else {
+      // do nothing
+    }
+  }
+
+  crossFade(sound1key, sound2key, dividingFactor){
+    var sound1 = this.game.add.audio(sound1key);
+    var sound2 = this.game.add.audio(sound2key);
+
+    if (!dividingFactor || dividingFactor < 1){
+      dividingFactor = 10;
+    }
+
+    sound1.play();
+
+    // Need to get duration AFTER play - this is only set when audio has been decoded! 
+    // Preload does not wait for audio to be decoded (grr) so no guarantee until play hit.
+    // Need milliseconds for setTimeout
+    var duration = sound1.totalDuration * 1000;
+    var fadeBoundary = duration / dividingFactor;
+    var tween = this.game.add.tween(sound1);
+    tween.to({ volume: 0 }, fadeBoundary, Phaser.Easing.Linear.None, false);
+
+    setTimeout(() => {
+      tween.start();
+      sound2.fadeIn(fadeBoundary);
+    }, duration - fadeBoundary);
+  }
+}
+
+module.exports = Audio;
---
+++ utils/audioLoop.js
@@ -0,0 +1,52 @@
+'use strict';
+
+class AudioLoop {
+  constructor(game, soundKey) {
+    this.game = game;
+    this.baseLoop = this.game.add.audio(soundKey);
+    this.baseLoop.loop = true;
+    this.playingSounds = [];
+  }
+
+  start() {
+    this.playingSounds.push(this.initialSound);
+    this.baseLoop.play();
+  }
+
+  // Adds an additional layer of sound at the point of the original sound's loop
+  addLayer(soundKey){
+    var newSound = this.game.add.audio(soundKey);
+    this.baseLoop.onLoop.addOnce(() => {
+      newSound.play();
+    });
+  }
+
+  newBaseSound(soundKey){
+    var newBaseSound = this.game.add.audio(soundKey);
+    newBaseSound.loop = true;
+    this.baseLoop.onLoop.addOnce((baseLoop) => {
+      baseLoop.stop();
+
+      newBaseSound.play();
+      this.baseLoop = newBaseSound;
+    });
+  }
+
+  stopAll(){
+    this.playingSounds.forEach((sound) => {
+      if (sound.isPlaying){
+        sound.stop();
+      }
+    });
+  }
+
+  fadeOutAll(fadeOutTime){
+    this.playingSounds.forEach((sound) => {
+      if (sound.isPlaying){
+        sound.fadeOut(fadeOutTime);
+      }
+    });
+  }
+}
+
+module.exports = AudioLoop;
